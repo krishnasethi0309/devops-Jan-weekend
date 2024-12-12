5fb9d01b1b4f6ceb34110b522273866eea1ac01f
krishnasethi0309 <31270509+krishnasethi0309@users.noreply.github.com>
2024-12-12 06:14:31 +0000

    Update Helloword.java

--- Helloword.java
+++ Helloword.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java world first");  
-     System.out.println("Hello Java world Second  3");  
+     System.out.println("Hello Java world Second  5");  
     }   
 }  
